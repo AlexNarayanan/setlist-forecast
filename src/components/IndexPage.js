3962cf490e4c088da6d9cc47e9747356deafbf2b
Alex Narayanan <spear4789876@gmail.com>
2017-05-24 20:49:56 -0400

    simple client/server communication

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -5,8 +5,8 @@ export default class IndexPage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {value: ''};
-        console.info('inside constructor');
+        this.state = { artist: '', response: '' };
+        console.info('inside constructor index page');
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -14,23 +14,45 @@ export default class IndexPage extends React.Component {
 
     handleChange(event) {
         event.preventDefault();
-        this.setState({value: event.target.value});
+        this.setState({ artist: event.target.value, response: this.state.response });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        console.info(this.state.value);
+        console.info(this.state.artist);
+
+        // Make xmlhttprequest
+        const xmlhttp = new XMLHttpRequest(),
+            _this = this;
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4) {
+                const response = JSON.parse(xmlhttp.responseText);
+                if (xmlhttp.status === 200 && response.status === 'OK') {
+                    _this.setState({ artist: '', response: response.artist });
+                } else {
+                    _this.setState({ artist: '', response: 'Sorry there has been an error' });
+                }
+            }
+        };
+        xmlhttp.open('POST', 'submit', true);
+        xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xmlhttp.send(`artist=${ this.state.artist }`);
     }
 
     render() {
         return (
-            <form method="post" action="/submit">
-                <label>
-                    Artist Name:
-                    <input type="text" name="artist" value={this.state.value} onChange={this.handleChange}/>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <div>
+                <form onSubmit={ this.handleSubmit }>
+                    <label>
+                        Artist Name:
+                        <input type="text" name="artist" value={ this.state.artist } onChange={ this.handleChange }/>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <div>
+                    <p>You have searched for: { this.state.response }</p>
+                </div>
+            </div>
         );
     }
 }
--- src/server.js
+++ src/server.js
@@ -57,9 +57,42 @@ app.get('*', (req, res) => {
     );
 });
 
-app.post("/submit", function(request, response){
-    const artistName = request.body.artist;
-    console.log(`The user searched for: ${artistName}`);
+app.post('/submit', (req, res) => {
+
+    const artist = req.body.artist;
+    console.log(`The user searched for: ${artist}`);
+    return res.send({ status: 'OK', artist: artist });
+    /*match(
+        { routes, location: req.url },
+        (err, redirectLocation, renderProps) => {
+            // in case of error display the error message
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+
+            // in case of redirect propagate the redirect to the browser
+            if (redirectLocation) {
+                return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+            }
+
+            // generate the React markup for the current route
+            let markup;
+            if (renderProps) {
+                // if the current route matched we have renderProps
+                markup = renderToString(<RouterContext {...renderProps } createElement={(Component, props) => {
+                    return <Component artist={artist} {...props} />;
+                }}/>);
+            } else {
+                // otherwise we can render a 404 page
+                markup = renderToString(<NotFoundPage/>);
+                res.status(404);
+            }
+
+            // render the index template with the embedded React markup
+            return res.render('index', { markup });
+
+        });*/
+
 });
 
 // start the server
